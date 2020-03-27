44ac464be75d573c2e9d5adf87446bfeaee9cb0f
ci7lus <7887955+ci7lus@users.noreply.github.com>
2020-03-28 01:36:08 +0900

    :art: 見た目をちょっと変更

--- src/index.tsx
+++ src/index.tsx
@@ -92,146 +92,167 @@ const App: React.FC<{}> = () => {
     }
   }
   return (
-    <div className="text-gray-800 mb-16">
-      <div className="bg-gray-800">
-        <div className="flex items-center container mx-auto flex justify-between max-w-screen-md text-gray-200">
-          <div className="flex items-center justify-start">
-            <a href="/">
-              <div className="flex items-center justify-start mx-4 my-3">
-                <h1
-                  className="font-bold text-lg"
-                  style={{ fontFamily: "Avenir Next" }}
-                >
-                  <img src={logo} width="100px" />
-                </h1>
-              </div>
-            </a>
+    <div className="min-h-screen w-full flex flex-col text-gray-800">
+      <div className="flex-1">
+        <div className=" bg-gray-800">
+          <div className="flex items-center container mx-auto flex justify-between max-w-screen-md text-gray-200">
+            <div className="flex items-center justify-start">
+              <a href="/">
+                <div className="flex items-center justify-start mx-4 my-3">
+                  <h1
+                    className="font-bold text-lg"
+                    style={{ fontFamily: "Avenir Next" }}
+                  >
+                    <img src={logo} width="100px" />
+                  </h1>
+                </div>
+              </a>
+            </div>
           </div>
+        </div>
+
+        <div className="container mx-auto max-w-screen-md p-4">
+          <form onSubmit={handleSubmitForm}>
+            <div className="flex flex-wrap -mx-3">
+              <div className="w-full px-3">
+                <label
+                  className="block tracking-wide text-gray-700 font-bold pb-2"
+                  htmlFor="timetable"
+                >
+                  タイムテーブル
+                </label>
+                <textarea
+                  id="timetable"
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight h-48"
+                  onChange={(e) => {
+                    setTimelineText(e.currentTarget.value)
+                  }}
+                  value={timelineText}
+                ></textarea>
+              </div>
+            </div>
+            <div className="flex items-center mb-6">
+              <div className="w-2/3">
+                <label
+                  className="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="carry_over"
+                >
+                  持ち越し時間
+                </label>
+              </div>
+              <div className="w-1/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight"
+                  id="carry_over"
+                  type="text"
+                  placeholder="1:10"
+                  value={carryOverText}
+                  onChange={(e) => {
+                    setCarryOverText(e.currentTarget.value)
+                  }}
+                />
+              </div>
+            </div>
+            <button
+              className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 mb-2 rounded"
+              type="submit"
+            >
+              変換
+            </button>
+            {err && <p className="text-gray-600 text-xs text-red-600">{err}</p>}
+          </form>
+          <div className="border-2 border-gray-200 my-4"></div>
+          {totalDamage && (
+            <div className="text-lg mb-2">トータルダメージ: {totalDamage}</div>
+          )}
+          <div className="w-full">
+            <div className="shadow overflow-hidden rounded border-b border-gray-200">
+              <table className="min-w-full bg-white">
+                <thead className="bg-gray-800 text-white">
+                  <tr>
+                    <th className="text-left py-3 px-4 font-semibold text-sm">
+                      デバフ
+                    </th>
+                    <th className="text-left py-3 px-4 font-semibold text-sm">
+                      時間
+                    </th>
+                    <th className="text-left py-3 px-4 font-semibold text-sm">
+                      キャラクター
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="text-gray-700">
+                  {timeline.map((line, idx) => {
+                    const remaining = line.remaining - carryOver
+                    let minute = Math.floor(remaining / 60)
+                    if (minute < 0) {
+                      minute = 0
+                    }
+                    let second = remaining % 60
+                    if (second < 0) {
+                      second = 0
+                    }
+                    return (
+                      <tr key={idx}>
+                        <td className="text-left py-1 px-4">{line.debuff}</td>
+                        <td className="text-left py-1 px-4">
+                          {minute}:{second.toString().padStart(2, "0")} (
+                          {remaining}秒)
+                        </td>
+                        <td className="text-left py-1 px-4">
+                          {line.character}
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          {generatedTimeline && (
+            <>
+              <textarea
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 my-3 leading-tight h-48"
+                readOnly={true}
+                value={generatedTimeline}
+              ></textarea>
+              <div className="flex justify-end">
+                <button
+                  className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 mb-2 rounded text-right"
+                  type="button"
+                  onClick={async () => {
+                    try {
+                      if (navigator.clipboard) {
+                        await navigator.clipboard.writeText(generatedTimeline)
+                        alert("コピーしました")
+                      }
+                    } catch (e) {
+                      console.error(e)
+                    }
+                  }}
+                >
+                  コピー
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
-      <div className="container mx-auto max-w-screen-md p-4">
-        <form onSubmit={handleSubmitForm}>
-          <div className="flex flex-wrap -mx-3">
-            <div className="w-full px-3">
-              <label
-                className="block tracking-wide text-gray-700 font-bold pb-2"
-                htmlFor="timetable"
-              >
-                タイムテーブル
-              </label>
-              <textarea
-                id="timetable"
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight h-48"
-                onChange={(e) => {
-                  setTimelineText(e.currentTarget.value)
-                }}
-                value={timelineText}
-              ></textarea>
-            </div>
-          </div>
-          <div className="flex items-center mb-6">
-            <div className="w-2/3">
-              <label
-                className="block text-gray-800 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                htmlFor="carry_over"
-              >
-                持ち越し時間
-              </label>
-            </div>
-            <div className="w-1/3">
-              <input
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight"
-                id="carry_over"
-                type="text"
-                placeholder="1:10"
-                value={carryOverText}
-                onChange={(e) => {
-                  setCarryOverText(e.currentTarget.value)
-                }}
-              />
-            </div>
-          </div>
-          <button
-            className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 mb-2 rounded"
-            type="submit"
-          >
-            変換
-          </button>
-          {err && <p className="text-gray-600 text-xs text-red-600">{err}</p>}
-        </form>
-        <div className="border-2 border-gray-200 my-4"></div>
-        {totalDamage && (
-          <div className="text-lg mb-2">トータルダメージ: {totalDamage}</div>
-        )}
-        <div className="w-full">
-          <div className="shadow overflow-hidden rounded border-b border-gray-200">
-            <table className="min-w-full bg-white">
-              <thead className="bg-gray-800 text-white">
-                <tr>
-                  <th className="text-left py-3 px-4 font-semibold text-sm">
-                    デバフ
-                  </th>
-                  <th className="text-left py-3 px-4 font-semibold text-sm">
-                    時間
-                  </th>
-                  <th className="text-left py-3 px-4 font-semibold text-sm">
-                    キャラクター
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="text-gray-700">
-                {timeline.map((line, idx) => {
-                  const remaining = line.remaining - carryOver
-                  let minute = Math.floor(remaining / 60)
-                  if (minute < 0) {
-                    minute = 0
-                  }
-                  let second = remaining % 60
-                  if (second < 0) {
-                    second = 0
-                  }
-                  return (
-                    <tr key={idx}>
-                      <td className="text-left py-1 px-4">{line.debuff}</td>
-                      <td className="text-left py-1 px-4">
-                        {minute}:{second.toString().padStart(2, "0")} (
-                        {remaining}秒)
-                      </td>
-                      <td className="text-left py-1 px-4">{line.character}</td>
-                    </tr>
-                  )
-                })}
-              </tbody>
-            </table>
+      <div className="bg-gray-800">
+        <div className="container mx-auto max-w-screen-md">
+          <div className="flex justify-end text-xs p-4 text-gray-200">
+            mugishuwa
+            {/**&nbsp;/&nbsp;
+            <a
+              className="text-blue-400"
+              target="_blank"
+              href="https://github.com/ci7lus/mugishuwa"
+              rel="noopener"
+            >
+              code
+            </a>**/}
           </div>
         </div>
-        {generatedTimeline && (
-          <>
-            <textarea
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 my-3 leading-tight h-48"
-              readOnly={true}
-              value={generatedTimeline}
-            ></textarea>
-            <div className="flex justify-end">
-              <button
-                className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 mb-2 rounded text-right"
-                type="button"
-                onClick={async () => {
-                  try {
-                    if (navigator.clipboard) {
-                      await navigator.clipboard.writeText(generatedTimeline)
-                      alert("コピーしました")
-                    }
-                  } catch (e) {
-                    console.error(e)
-                  }
-                }}
-              >
-                コピー
-              </button>
-            </div>
-          </>
-        )}
       </div>
     </div>
   )
